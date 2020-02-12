79ce1015001a08e74dddca75cb07dedc674c088b
root <root@vps787765.ovh.net>
2020-02-12 17:18:05 +0100

    Socket and db communication

--- public/maptest.js
+++ public/maptest.js
@@ -1,7 +1,11 @@
+//initialise la carte
 var mymap = L.map('gamemap').setView([45.785, 4.8778], 15);
+
+//Initialise le socket
 var socket = io();
 
 
+//Osef puissance 3000 verbeux : charge juste une carte en fond avec une clef d'API
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery  <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 20,
@@ -9,6 +13,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
         accessToken: 'pk.eyJ1IjoiYW50b2luZXJjYnMiLCJhIjoiY2s2Nm5hdzh2MTExOTNtbXBsbG1qbjc1NSJ9.lyYYPYUi6_L9nsdvNcRudw'
 }).addTo(mymap);
 
+//Fonction ajoutant un point à la carte avec cette couleur et ces positions latitude/longitude
 function addPointToMap(color, location) {
 	L.circle(location, {
         	color: color,
@@ -18,10 +23,12 @@ function addPointToMap(color, location) {
 	}).addTo(mymap);
 }
 
+//ajoute le point à la carte lorsqu'on a une maj
 socket.on('points_updated', function(item) {
-	addPointToMap(item.color, item.location);
+	addPointToMap(item.new_val.color, item.new_val.location);
 });
 
+//ajoute un point à la carte lors d'un chargement initial
 socket.on('points_loaded', function(item) {
 	addPointToMap(item.color, item.location);
 });
--- server.js
+++ server.js
@@ -1,33 +1,66 @@
+//gestion serveur http
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
+
+//init socket.io
 var io = require('socket.io')(http);
+
+//init API ReQL
 var r = require("rethinkdb");
 
+//Connexion avec la BD
+var dbconn;
 
+
+
+//On fourni au client (web) tous les fichiers dans le dossier "public"
 app.use(express.static('public'));
 
-//Quand un client se connecte, on lui envoie tous les points de la carte
-io.on('connection', function(socket){ 
-	console.log('a user connected');
-	r.connect({db: 'test'}).then(function(conn) {
-		r.table('points').run(conn, function(err, cursor) {
-			cursor.each(function(err, item) {
-				socket.emit("points_loaded", item); //On n'envoie qu'à ce client
-			});
-		});
-	});
-});
-
+//On ouvre une connexion http sur le port 8000
 http.listen(8000, function(){
 	  console.log('listening on *:8000');
 });
 
+
+
+//On se connecte à la BD 'test' sur notre serveur en local
+r.connect({host: 'localhost', port: 28015, db: 'test'})
+        .then(conn => {
+        	dbconn = conn;
+		scopeForChanges();
+});
+
+
 //Dès qu'un changement intervient sur les points, on envoie la modification à tout le monde.
-r.connect({db: 'test'}).then(function(conn) {
-	r.table('points').changes().run(conn, function(err, cursor) {
+function scopeForChanges() {
+	r.table('points').changes().run(dbconn, function(err, cursor) {
 		cursor.each(function(err, item) {
+			console.log("New point detected !");
 			io.emit("points_updated", item); //On envoie à tout le monde
 		});
 	});
+}
+
+
+//Quand un client se connecte
+io.on('connection', function(socket) { 
+  	console.log('New connection !');
+	//On envoie tous les points de la carte à ce client
+	r.table('points').run(dbconn, function(err, cursor) {
+			cursor.each(function(err, item) {
+			socket.emit("points_loaded", item); //On n'envoie qu'à ce client
+		});
+	});
+	//Quand ce client enverra un "new_point", on l'ajoute à la BD
+	socket.on('new_point', function(data) {
+		console.log(data.location);
+		r.table('points').insert({"color": "#FF54EF", "location": data.location}).run(dbconn, function(err, result) {
+    			if (err) throw err;
+    			//console.log(JSON.stringify(result, null, 2));
+		});
+	});
 });
+
+
+
