31f6f99dbea97f309a83cb619725a5185ebe2ff2
antoinekia <antoinekia@gmail.com>
2020-02-24 14:36:22 +0100

    Map + lines

--- PainttheWorldApp/app/src/main/java/fr/insalyon/painttheworldapp/MainActivity.java
+++ PainttheWorldApp/app/src/main/java/fr/insalyon/painttheworldapp/MainActivity.java
@@ -2,15 +2,27 @@ package fr.insalyon.painttheworldapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
@@ -18,18 +30,34 @@ import com.github.nkzawa.socketio.client.Socket;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.IMyLocationConsumer;
+import org.osmdroid.views.overlay.mylocation.IMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, IMyLocationConsumer {
+
+    private MapView mMapView;
+    private MyLocationNewOverlay mLocationOverlay;
+    private GpsMyLocationProvider mLocationProvider;
 
     private TextView mCoordTextView;
     private Button mUpdateButton;
-    private FusedLocationProviderClient fusedLocationClient;
+    private Button mBeginButton;
+
+    private boolean isRecording;
+
 
     private Socket mSocket;
 
     public static final String SERVER_URL = "http://paint.antoine-rcbs.ovh:8000";
+
+    private ArrayList<GeoPoint> lineLocations;
+
+
 
     @Override
     /**
@@ -38,13 +66,41 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.lineLocations = new ArrayList<GeoPoint>();
+
         //instanciation des boutons et textes
-        mCoordTextView = findViewById(R.id.text_view);
-        mUpdateButton = findViewById(R.id.button);
-        mUpdateButton.setOnClickListener(this);
+
+
+        final Context context = getApplicationContext();
+        Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context));
+
+
+
+        mMapView = findViewById(R.id.map);
+        mMapView.setTileSource(TileSourceFactory.MAPNIK);
+        mMapView.setBuiltInZoomControls(false);
+        mMapView.setMultiTouchControls(true);
+        IMapController mapController = mMapView.getController();
+        mapController.setZoom(18);
+        GeoPoint startPoint = new GeoPoint(45.7837763, 4.872973);
+        mapController.setCenter(startPoint);
+        this.mLocationProvider = new GpsMyLocationProvider(context);
+        this.mLocationProvider.startLocationProvider(this);
+        mLocationProvider.setLocationUpdateMinDistance(3);
+        mLocationProvider.setLocationUpdateMinTime(1000);
+        this.mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(context), mMapView);
+        this.mLocationOverlay.enableMyLocation();
+        this.mLocationOverlay.enableFollowLocation();
+        mMapView.getOverlays().add(this.mLocationOverlay);
+
 
         //Instanciation du service de localisation
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        mCoordTextView = findViewById(R.id.text_view);
+        mUpdateButton = findViewById(R.id.push_button);
+        mBeginButton = findViewById(R.id.begin_button);
+        mUpdateButton.setOnClickListener(this);
+        mBeginButton.setOnClickListener(this);
 
         //Instanciation du socket avec le serveur node.js
         try {
@@ -54,7 +110,23 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         }
         mSocket.connect();
 
+
+
+
+
+
     }
+
+    public void onResume(){
+        super.onResume();
+       mMapView.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    public void onPause(){
+        super.onPause();
+        mMapView.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+    }
+
 
     @Override
     /**
@@ -62,20 +134,35 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
      * et on l'envoie par le biais du socket au serveur
      */
     public void onClick(View v) {
-        fusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            //On affiche sur le label les coordonnées GPS
-                            String message = location.getLatitude() + " | " +  location.getLongitude();
-                            mCoordTextView.setText(message);
-                            //On envoie ces nouvelles coordonnées au serveur au format JSON
-                            mSocket.emit("new_point", locationToJSON(location));
-                        }
-                    }
-                });
+        if (v.equals(mBeginButton)) {
+            Toast.makeText(getApplicationContext(), "Début du dessin !", Toast.LENGTH_LONG).show();
+            this.lineLocations.clear();
+            this.isRecording = true;
+        } else if (v.equals(mUpdateButton)) {
+            Toast.makeText(getApplicationContext(), "Upload de ce trait !", Toast.LENGTH_SHORT).show();
+            mSocket.emit("new_line", lineToJSON(this.lineLocations, 0xff0000));
+            this.isRecording = false;
+        }
+    }
+
+
+    private JSONObject lineToJSON(ArrayList<GeoPoint> pointList, int color) {
+        JSONObject json = new JSONObject();
+        JSONArray pointsArray = new JSONArray();
+
+        try {
+            json.put("color", color);
+            for (GeoPoint geoPoint : pointList) {
+                JSONArray point = new JSONArray();
+                point.put(geoPoint.getLatitude());
+                point.put(geoPoint.getLongitude());
+                pointsArray.put(point);
+            }
+            json.put("location", pointsArray);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return json;
     }
 
     /**
@@ -95,5 +182,17 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
             e.printStackTrace();
         }
         return point;
+    }
+
+    @Override
+    public void onLocationChanged(Location location, IMyLocationProvider source) {
+        if (location != null && this.isRecording) {
+            //On affiche sur le label les coordonnées GPS
+            String message = lineLocations.size() + " : " + location.getLatitude() + " | " +  location.getLongitude();
+            mCoordTextView.setText(message);
+            this.lineLocations.add(new GeoPoint(location));
+            //On envoie ces nouvelles coordonnées au serveur au format JSON
+            //mSocket.emit("new_point", locationToJSON(location));
+        }
     }
 }
