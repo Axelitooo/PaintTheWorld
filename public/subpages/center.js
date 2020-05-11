e48ae2a302fa3d938117c8733c6431f3f42d35d3
axelito <axel.rosenstiehl@insa-lyon.fr>
2020-05-11 15:48:33 +0200

    putting buttons in a propper list

--- public/subpages/center.js
+++ public/subpages/center.js
@@ -1,7 +1,9 @@
 function add(buttonNumber, drawing) {
+	var element = document.createElement("LI");
 	var button = document.createElement("BUTTON");
 	button.innerHTML = "I am button " + buttonNumber;
-	document.getElementById("menu").appendChild(button);
+	element.appendChild(button);
+	document.getElementById("list").appendChild(element);
 	button.onclick = function () {
 		mymap.setView([drawing.lat, drawing.lng], 19);
 	}
--- public/subpages/drawings.js
+++ public/subpages/drawings.js
@@ -1,5 +1,5 @@
 getPlayerDrawings("RootUser42");
-var inc = 0
+var inc = 1;
 socket.on('player_drawings_loaded', function(drawing) {
 	add(inc++, drawing);
 });
