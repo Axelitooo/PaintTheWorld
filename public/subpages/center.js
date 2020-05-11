5495ecccf31d8e29930353bba40816dccc521ba9
axelito <axel.rosenstiehl@insa-lyon.fr>
2020-05-11 14:59:16 +0200

    attempt at creating user drawings menu

--- public/subpages/center.js
+++ public/subpages/center.js
@@ -1,7 +1,7 @@
 function add(button, buttonNumber, drawing) {
 	var button = document.createElement("BUTTON");
 	button.innerHTML = "I am button &{buttonNumber}";
-	menu.appendChild(button);
+	getElementById("menu").appendChild(button);
 	button.onclick = centerOnDrawing(); // Note this is a function
 	
 function centerOnDrawing() {
